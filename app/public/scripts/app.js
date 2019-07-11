acb90721914870d375d13452e03b8e78219757c0
Florent DUVEAU <florent.duveau@kisskissbankbank.com>
2019-07-11 07:57:23 +0200

    react-hot-loader is a dep

--- app/public/scripts/app.js
+++ app/public/scripts/app.js
@@ -14,13 +14,10 @@ var _index = _interopRequireDefault(require("./app/index"));
 
 var _store = _interopRequireDefault(require("./store"));
 
-var _test = _interopRequireDefault(require("./test.json"));
-
 var __signature__ = typeof reactHotLoaderGlobal !== 'undefined' ? reactHotLoaderGlobal.default.signature : function (a) {
   return a;
 };
 
-console.log(_test.default);
 (0, _reactDom.hydrate)(_react.default.createElement(_reactRouterDom.BrowserRouter, null, _react.default.createElement(_reduxReactHook.StoreContext.Provider, {
   value: _store.default
 }, _react.default.createElement(_index.default, null))), document.getElementById('body'));
