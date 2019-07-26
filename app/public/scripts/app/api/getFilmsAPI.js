019e1dcb19512cd30d763a8ad14f0c85cc08e1f5
Florent DUVEAU <florent.duveau@kisskissbankbank.com>
2019-07-26 21:13:53 +0200

    use IndexedDB to store data

--- app/public/scripts/app/api/getFilmsAPI.js
+++ app/public/scripts/app/api/getFilmsAPI.js
@@ -1,13 +1,34 @@
 "use strict";
 
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.getFilmsAPI = void 0;
 
+var _dexie = _interopRequireDefault(require("dexie"));
+
+var _isEmpty = _interopRequireDefault(require("lodash/fp/isEmpty"));
+
+const db = new _dexie.default('reactredux-fr-db');
+db.version(1).stores({
+  films: '++id,title,image'
+});
+
 const getFilmsAPI = async () => {
-  const response = await fetch('https://startmeup.herokuapp.com/api/movies');
-  return await response.json();
+  const localFilms = await db.films.toArray();
+
+  if (!navigator.onLine) {
+    return localFilms;
+  } else if ((0, _isEmpty.default)(localFilms)) {
+    const response = await fetch('https://startmeup.herokuapp.com/api/movies');
+    const films = await response.json();
+    await db.films.bulkPut(films);
+    return films;
+  } else fetch('https://startmeup.herokuapp.com/api/movies').then(response => response.json()).then(films => db.films.bulkPut(films));
+
+  return localFilms;
 };
 
 exports.getFilmsAPI = getFilmsAPI;
--- public/scripts/app/api/getFilmsAPI.js
+++ public/scripts/app/api/getFilmsAPI.js
@@ -1,4 +1,21 @@
+import Dexie from 'dexie';
+import isEmpty from 'lodash/fp/isEmpty';
+
+const db = new Dexie('reactredux-fr-db');
+db.version(1).stores({ films: '++id,title,image' });
+
 export const getFilmsAPI = async () => {
-  const response = await fetch('https://startmeup.herokuapp.com/api/movies');
-  return await response.json();
+  const localFilms = await db.films.toArray();
+  if (!navigator.onLine) {
+    return localFilms;
+  } else if (isEmpty(localFilms)) {
+    const response = await fetch('https://startmeup.herokuapp.com/api/movies');
+    const films = await response.json();
+    await db.films.bulkPut(films);
+    return films;
+  } else
+    fetch('https://startmeup.herokuapp.com/api/movies')
+      .then(response => response.json())
+      .then(films => db.films.bulkPut(films));
+  return localFilms;
 };
